bd4a5c9df9d67103dd975542e2b140885cd90128
yagnik0 <yashyagnik456@.com>
2023-11-26 10:18:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const clock = document.getElementById('clock')
+// we can also use 
+// const clock = document.querySelector('#clock)
+
+
+
+
+// setInterval :- it takes method and interval, which runs continuously 
+
+setInterval(function(){
+    let date = new Date()
+    clock.innerHTML = date.toLocaleTimeString()
+})
